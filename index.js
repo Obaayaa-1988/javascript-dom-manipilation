1ef89f54d74e85be87dd7171961a29674dd2e5ac
Abigail <ashbella333@gmail.com>
2022-08-09 16:56:44 +0000

    added document model classname

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // alert("hello world")
+
+const bookTitles = document.getElementsByClassName('title')
+//turning an html collection into an array
+Array.from(bookTitles).forEach(element => {
+    console.log(element)
+});
+
+
+
