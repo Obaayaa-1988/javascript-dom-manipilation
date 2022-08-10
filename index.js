fbc0481a2cea490bc0f196a92343152e958937bf
Abigail <ashbella333@gmail.com>
2022-08-10 17:06:29 +0000

    added text nontent

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 // alert("hello world")
 
-const bookTitles = document.getElementsByClassName('title')
-//turning an html collection into an array
-Array.from(bookTitles).forEach(element => {
-    console.log(element)
-});
+// const books = document.querySelectorAll('title')
+// //turning an html collection into an array
+// Array.from(books).forEach(element => {
+//     console.log(element.textContent)
+// });
 
 
+//to get the content of the div-id(will bring everything in the div), li(will bring everything inside the li tag including the span) class(.name will bring the content of name class)
+// const bookTitles = document.querySelectorAll('#book-list li .name')
+// Array.from(bookTitles).forEach(books => {
+//     books.textContent += " helloooo  there"//appending a string to the html element inside javascript the hello there will attach each of the book name
+// });
 
+//updating a jav
