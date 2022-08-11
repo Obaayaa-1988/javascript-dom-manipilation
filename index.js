1436056479a2e27e8eb5cf84f14df55e6c67da3f
Abigail <ashbella333@gmail.com>
2022-08-11 16:59:51 +0000

    updating an html element with innerHTML

--- index.js
+++ index.js
@@ -13,4 +13,28 @@
 //     books.textContent += " helloooo  there"//appending a string to the html element inside javascript the hello there will attach each of the book name
 // });
 
-//updating a jav
+//updating a javascript elements
+
+
+// const books = document.querySelector('#book-list')
+//to replace all the existing html in that #book-list div
+// books.innerHTML = '<h2>More books</h2>'
+//to append a new html to #book-list div
+// books.innerHTML += "<h1>more books more books</h1>"
+
+
+//DOM NODES
+// const banner = document.querySelector("#page-banner")
+// console.log("#page-banner node type is: ", banner.nodeType)//node type
+// console.log("#page-banner node name is: ", banner.nodeName)//node name is a div
+// console.log("#page-banner has child node: ", banner.childNodes)//child nodes
+
+//we als check to see if it has child nodes, meaning if it has other html elements inside that div
+
+//banner(#page-banner) node type is 1 meanin it is an html element
+
+//TRAVERSING IN THE DOM
+//for example checking to see if a selected div has a parent div
+
+const list = document.querySelector("#book-list")
+console.log("list is parent is: ", list.parentNode)
